17eab319da3e00043765fecbd7fd26e08ceab49d
Ebba-Ene <eneqvist.ebba@gmail.com>
2026-04-27 13:56:04 +0200

    feature(playlist): implemented the ability to share playlist, changed user to owner

--- backend/middleware/ownership.js
+++ backend/middleware/ownership.js
@@ -6,7 +6,7 @@ export const isPlaylistOwner = async (req, res, next) => {
     console.error('Ownership: Playlist not found');
     return res.status(404).json({ error: 'Playlist not found' });
   }
-  if (!playlist.user || !playlist.user.equals(req.user._id)) {
+  if (!playlist.owner || !playlist.owner.equals(req.user._id)) { //Kanske ska byta till owner
     console.error('Ownership: Not authorized to modify this playlist');
     return res.status(403).json({ error: 'Not authorized to modify this playlist' });
   }
@@ -21,4 +21,18 @@ export const isPlaylistOwner = async (req, res, next) => {
  */
 export const isPlaylistSharedWithUser = async (req, res, next) => {
   // TODO: Implement this (student / course extension)
+  const playlist = await Playlist.findById(req.params.id);
+  if (!playlist) {
+    console.error('Ownership: Shared Playlist not found');
+    return res.status(404).json({ error: 'Playlist not found' });
+  }
+
+  const isUserInList = playlist.users.some(userId => userId.equals(req.user._id));
+
+  if (!isUserInList && playlist.owner.toString() !== (req.user._id.toString())) {
+    console.error('Ownership: Not authorized to see this playlist');
+    return res.status(403).json({ error: 'Not authorized to see this playlist' });
+  }
+  req.playlist = playlist;
+  next();
 };
--- backend/routes/playlists.js
+++ backend/routes/playlists.js
@@ -1,13 +1,13 @@
 import { Router } from 'express';
 import Playlist from '../models/Playlist.js';
 import { requireAuth } from '../middleware/auth.js';
-import { isPlaylistOwner } from '../middleware/ownership.js';
+import { isPlaylistOwner, isPlaylistSharedWithUser } from '../middleware/ownership.js';
 
 const router = Router();
 
 router.get('/latest', async (req, res) => {
   try {
-    const playlists = await Playlist.find({ user: null })
+    const playlists = await Playlist.find({ owner: null })
       .sort({ _id: -1 })
       .limit(5)
       .populate('songs', 'title');
@@ -24,9 +24,9 @@ router.get('/latest', async (req, res) => {
  */
 router.get('/my', requireAuth, async (req, res) => {
   try {
-    const playlists = await Playlist.find({ user: req.user._id })
+    const playlists = await Playlist.find({ owner: req.user._id })
       .populate('songs', 'title artist durationSeconds')
-      .populate('user', 'email');
+      .populate('owner', 'email');
     res.json(playlists);
   } catch (err) {
     console.error('My playlists failed:', err.message);
@@ -40,10 +40,10 @@ router.get('/my', requireAuth, async (req, res) => {
 router.get('/', async (req, res) => {
   try {
     const playlists = await Playlist.find({
-      user: null,
+      owner: null,
     })
       .populate('songs', 'title artist durationSeconds')
-      .populate('user', 'email');
+      .populate('owner', 'email');
     res.json(playlists);
   } catch (err) {
     console.error('Playlists list failed:', err.message);
@@ -51,17 +51,30 @@ router.get('/', async (req, res) => {
   }
 });
 
+router.get('/my/sharedPlaylists/:id', requireAuth, isPlaylistSharedWithUser, async (req, res) => {
+  try{
+    console.log("Hej")
+    const playlists = await Playlist.find({ users: req.user._id })
+      .populate('songs', 'title artist durationSeconds')
+      .populate('owner', 'email').lean()
+    res.json(playlists);
+  }catch (err) {
+    console.error('Shared playlists failed:', err.message);
+    res.status(500).json({ error: err.message });
+  }
+})
+
 router.post('/my', requireAuth, async (req, res) => {
   try {
     const body = {
       name: req.body.name,
       description: req.body.description,
       songs: req.body.songs || [],
-      user: req.body.user ?? req.user._id,
+      owner: req.body.ower ?? req.user._id,
     };
     const playlist = await Playlist.create(body);
     await playlist.populate('songs', 'title artist durationSeconds');
-    await playlist.populate('user', 'email');
+    await playlist.populate('owner', 'email');
     res.status(201).json(playlist);
   } catch (err) {
     console.error('Create playlist failed:', err.message);
@@ -77,7 +90,7 @@ router.put('/my/:id', requireAuth, isPlaylistOwner, async (req, res) => {
       { new: true, runValidators: true },
     )
       .populate('songs', 'title artist durationSeconds')
-      .populate('user', 'email');
+      .populate('owner', 'email');
     if (!playlist) {
       console.error('Update playlist: Playlist not found');
       return res.status(404).json({ error: 'Playlist not found' });
@@ -88,6 +101,27 @@ router.put('/my/:id', requireAuth, isPlaylistOwner, async (req, res) => {
     res.status(400).json({ error: err.message });
   }
 });
+
+//Owner can add users
+router.patch('/my/:id', requireAuth, isPlaylistOwner, async (req,res) => {
+    try {
+      const playlist = await Playlist.findByIdAndUpdate(
+        req.params.id,
+        { $addToSet: { users: req.body.user } },
+        { new: true, runValidators: true },
+      )
+        .populate('songs', 'title artist durationSeconds')
+        .populate('owner', 'email');
+      if (!playlist) {
+        console.error('Update playlist: Playlist not found');
+        return res.status(404).json({ error: 'Playlist not found' });
+      }
+      res.json(playlist);
+  } catch (err) {
+    console.error('Update playlist failed:', err.message);
+    res.status(400).json({ error: err.message });
+  }
+})
 
 router.delete('/my/:id', requireAuth, isPlaylistOwner, async (req, res) => {
   try {
@@ -114,7 +148,7 @@ router.get('/:id', async (req, res) => {
   try {
     const playlist = await Playlist.findOne({
       _id: req.params.id,
-      user: null,
+      owner: null,
     })
       .populate({ path: 'songs', populate: { path: 'artist', select: 'name' } });
     if (!playlist) {
